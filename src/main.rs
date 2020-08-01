77d8e31fe7fb8bedf0acdd0f06cf10a32f8ddcb1
tokizuoh <k_sakai@yumemi.co.jp>
2020-08-02 00:48:10 +0900

    AtCoderの問題一覧を取得（文字列）

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use async_std::task;
+
+// struct Problem {
+//     id: String,
+//     contest_id: String,
+//     title: String
+// }
+
+fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+    task::block_on(async {
+        let mut res = surf::get("https://kenkoooo.com/atcoder/resources/problems.json").await?;
+        dbg!(res.body_string().await?);
+        let body = res.body_string().await?;
+        println!("{}", body);
+        Ok(())
+    })
 }
